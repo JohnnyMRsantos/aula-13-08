eeb4aa724610d477bd3e7410cbdb259db6a7d49e
Johnny Miguel <159183909+JohnnyMRsantos@users.noreply.github.com>
2024-08-13 13:46:14 -0300

    Add files via upload

---
+++ Comandos SQL - Restrições.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE alunos(
+	ID int(3) UNIQUE,
+  	Nome varchar(30)NOT NULL,
+  	Idade varchar(3),
+ 	Turma varchar(4)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
